3fd7afa1e3cbc14f0332497ab86b4a7cfaf4a387
hsmak <3461124+hsmak@users.noreply.github.com>
2019-02-25 18:16:08 -0500

    committing changes..

--- spark-sql/src/main/scala/org/hsmak/_foundations/RDDRunner.scala
+++ spark-sql/src/main/scala/org/hsmak/_foundations/RDDRunner.scala
@@ -41,6 +41,29 @@ object RDDRunner {
     dfFromParRDD.show
 
 
+
+
+    // TODO - to be tested
+    import scala.io.Source._
+
+//    read a json file from a url stream
+    val json = fromURL("https://raw.githubusercontent.com/databricks/Spark-The-Definitive-Guide/master/data/flight-data/json/2010-summary.json#")
+      .mkString
+      .split("\n")// since it's a multi-line json
+
+
+    // convert Json string to RDD
+    val rddJson = sc.parallelize(json, 8)
+    //convert jsonRDD -> DS -> DF
+    val df = spark.read.option("header", true).option("inferSchema", true).json(rddJson.toDS)
+
+
+    import org.apache.spark.sql.functions._
+    df.groupBy('DEST_COUNTRY_NAME).agg(sum('count) as 'sum).select("*").sort('sum desc).show
+
+    df.createOrReplaceTempView("flights")
+
+    spark.sql("SELECT DEST_COUNTRY_NAME, sum(count) as sum FROM flights GROUP BY DEST_COUNTRY_NAME ORDER BY sum DESC LIMIT 5").show
     spark.stop()
   }
 
