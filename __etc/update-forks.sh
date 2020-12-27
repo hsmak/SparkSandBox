cf5c37ec7090dfe1dc5843bd3adcc63c7d52db43
hsmak <husain.alkhamis@gmail.com>
2020-12-27 14:35:53 -0600

    committing changes..

---
+++ __etc/update-forks.sh
@@ -0,0 +1,2 @@
+git subtree pull --prefix _forks/spark-training https://github.com/TedBear42/spark_training.git master
+git subtree pull --prefix _forks/spark-scala-examples https://github.com/spark-examples/spark-scala-examples.git master
